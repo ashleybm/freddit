f96086f0c5c8006c2592c20844a59b0c98dc7e72
Ashley Baker <ashleybm@umich.edu>
2019-04-21 21:27:39 -0400

    fix preference manager

--- app/src/main/java/ashleybaker/freddit_v1/MainActivity.java
+++ app/src/main/java/ashleybaker/freddit_v1/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.SharedPreferences;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -14,6 +15,7 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.preference.PreferenceManager;
 
 public class MainActivity extends AppCompatActivity {
 
